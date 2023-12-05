a946f55e45a3374703d23f48ec55e0aa1f50914d
Zense <73891112+Zenolitee@users.noreply.github.com>
2023-12-06 00:26:52 +0800

    [WORKING] Cryptography so far

--- Cryptography.py
+++ Cryptography.py
@@ -9,6 +9,9 @@ def caesar_encrypt(text, shift):
             encrypted_text += char
     return encrypted_text
 
+def caesar_decrypt(text, shift):
+    return caesar_encrypt(text, -shift)
+
 def vigenere_encrypt(text, key):
     encrypted_text = ""
     key_length = len(key)
@@ -22,6 +25,21 @@ def vigenere_encrypt(text, key):
         else:
             encrypted_text += char
     return encrypted_text
+
+def vigenere_decrypt(text, key):
+    decrypted_text = ""
+    key_length = len(key)
+    for i, char in enumerate(text):
+        if char.isalpha():
+            start = ord('a') if char.islower() else ord('A')
+            key_char = key[i % key_length].lower()
+            key_shift = ord(key_char) - ord('a')
+            decrypted_char = chr((ord(char) - start - key_shift) % 26 + start)
+            decrypted_text += decrypted_char
+        else:
+            decrypted_text += char
+    return decrypted_text
+
 
 def reverse_text(text):
     return text[::-1]
@@ -48,15 +66,60 @@ def encrypt_file(input_file_path, output_file_path, vigenere_key):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Get input file name from user
-input_file_name = input("Enter the name of the text file to encrypt (including extension): ")
-input_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{input_file_name}'
+def decrypt_file(input_file_path, output_file_path, vigenere_key):
+    try:
+        with open(input_file_path, 'r') as file:
+            content = file.read()
+            first_word_length = len(content.split()[0])
+            caesar_shift_1 = first_word_length
+            caesar_shift_2 = first_word_length  # Use the same shift as the previous Caesar Cipher
+            reversed_content = reverse_text(content)
+            vigenere_decrypted_content = caesar_decrypt(reversed_content, caesar_shift_2)
+            caesar_decrypted_content = vigenere_decrypt(vigenere_decrypted_content, vigenere_key)
+            final_decrypted_content = caesar_decrypt(caesar_decrypted_content, caesar_shift_1)
+            
+        with open(output_file_path, 'w') as file:
+            file.write(final_decrypted_content)
+        
+        print(f"Decryption successful. Decrypted content saved to {output_file_path}")
 
-# Generate output file name based on input file name
-output_file_name = f'Decrypted_{input_file_name}'
-output_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{output_file_name}'
+    except FileNotFoundError:
+        print(f"Error: The file '{input_file_path}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# Get Vigenere key from user
-vigenere_key = input("Enter the Vigenere key: ")
+# Get user input for encryption or decryption
+action = input("Do you want to (E)ncrypt or (D)ecrypt? ").upper()
 
-encrypt_file(input_file_path, output_file_path, vigenere_key)
+if action == 'E':
+    # Encryption process
+    # Get input file name from user
+    input_file_name = input("Enter the name of the text file to encrypt (including extension): ")
+    input_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{input_file_name}'
+
+    # Generate output file name based on input file name
+    output_file_name = f'Encrypted_{input_file_name}'
+    output_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{output_file_name}'
+
+    # Get Vigenere key from user
+    vigenere_key = input("Enter the Vigenere key: ")
+
+    encrypt_file(input_file_path, output_file_path, vigenere_key)
+
+elif action == 'D':
+    # Decryption process
+    # Get input file name from user
+    input_file_name = input("Enter the name of the text file to decrypt (including extension): ")
+    input_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{input_file_name}'
+
+    # Generate output file name based on input file name
+    output_file_name = f'Decrypted_{input_file_name}'
+    output_file_path = f'C:\\Users\\Zen\\Desktop\\Cryptography\\Cryptography\\{output_file_name}'
+
+    # Get Vigenere key from user
+    vigenere_key = input("Enter the Vigenere key: ")
+
+    decrypt_file(input_file_path, output_file_path, vigenere_key)
+
+else:
+    print("Invalid option. Please enter 'E' for encryption or 'D' for decryption.")
